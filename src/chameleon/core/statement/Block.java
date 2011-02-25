9bd5dd0e021b0365ff883e27aa669db586e1670a
unknown <Jens@.(none)>
2011-02-25 19:20:56 +0100

    Revert "Implementing aspect parameters"
    This reverts commit d25cb910bcd08443a6fa528f29d916945c659c1e.

--- src/chameleon/core/statement/Block.java
+++ src/chameleon/core/statement/Block.java
@@ -15,96 +15,78 @@ import chameleon.exception.ChameleonProgrammerException;
 /**
  * @author Marko van Dooren
  */
-public class Block extends StatementImpl<Block> implements
-		StatementListContainer<Block> {
-	// TODO: should this be a member, or should there be a separate
-	// ObjectInitializer that contains a block ?
-	// TODO: can any statement be a member ? In this case the methods of Member
-	// have to move up.
+public class Block extends StatementImpl<Block> implements StatementListContainer<Block> {
+	//TODO: should this be a member, or should there be a separate ObjectInitializer that contains a block ?
+	//TODO: can any statement be a member ? In this case the methods of Member have to move up.
 
 	/**
 	 * STATEMENTS
 	 */
-	private OrderedMultiAssociation<Block, Statement> _statements = new OrderedMultiAssociation<Block, Statement>(
-			this);
+	private OrderedMultiAssociation<Block,Statement> _statements = new OrderedMultiAssociation<Block,Statement>(this);
 
-	public OrderedMultiAssociation<Block, Statement> getStatementsLink() {
-		return _statements;
-	}
+  public OrderedMultiAssociation<Block,Statement> getStatementsLink() {
+    return _statements;
+  }
 
-	public void addStatement(Statement statement) {
-		if (statement != null) {
-			_statements.add(statement.parentLink());
-		}
-	}
+  public void addStatement(Statement statement) {
+  	if(statement != null) {
+      _statements.add(statement.parentLink());
+  	}
+  }
 
-	public void removeStatement(Statement statement) {
-		if (statement != null) {
-			_statements.remove(statement.parentLink());
-		}
-	}
+  public void removeStatement(Statement statement) {
+  	if(statement != null) {
+      _statements.remove(statement.parentLink());
+  	}
+  }
 
-	public List<Statement> statements() {
-		return _statements.getOtherEnds();
-	}
+  public List<Statement> statements() {
+    return _statements.getOtherEnds();
+  }
 
-	public Block clone() {
-		final Block result = new Block();
-		new Visitor<Statement>() {
-			public void visit(Statement element) {
-				result.addStatement(element.clone());
-			}
-		}.applyTo(statements());
-		return result;
-	}
+  public Block clone() {
+    final Block result = new Block();
+    new Visitor<Statement>() {
+      public void visit(Statement element) {
+        result.addStatement(element.clone());
+      }
+    }.applyTo(statements());
+    return result;
+  }
 
-	public List<? extends Element> children() {
-		return statements();
-	}
+  public List<? extends Element> children() {
+    return statements();
+  }
 
-	public int getIndexOf(Statement statement) {
-		return statements().indexOf(statement) + 1;
-	}
-
+  public int getIndexOf(Statement statement) {
+    return statements().indexOf(statement) + 1;
+  }
+  
 	public List<Statement> statementsAfter(Statement statement) {
-		List<Statement> statements = statements();
+		List<Statement> statements = statements(); 
 		int index = statements.indexOf(statement);
-		// returns a view on a clone of _statements (getStatements() clones the
-		// list).
-		// the view depends on the local variable, but since no other references
-		// exist
+		// returns a view on a clone of _statements (getStatements() clones the list).
+		// the view depends on the local variable, but since no other references exist
 		// this is not a problem.
 		return statements.subList(index, statements.size());
 	}
 
-	/*
-	 * @
-	 * 
-	 * @ public behavior
-	 * 
-	 * @
-	 * 
-	 * @ post getStatements().indexOf(element) == 0) ==> \result ==
-	 * parent().lexicalContext(this);
-	 * 
-	 * @ post getStatements().indexOf(Element) > 0) ==>
-	 * 
-	 * @ \result == getStatements().elementAt(getStatements().indexOf(element) -
-	 * 1).lexicalContext();
-	 * 
-	 * @
-	 */
-	public LookupStrategy lexicalLookupStrategy(Element element)
-			throws LookupException {
+ /*@
+   @ public behavior
+   @
+   @ post getStatements().indexOf(element) == 0) ==> \result == parent().lexicalContext(this);
+   @ post getStatements().indexOf(Element) > 0) ==> 
+   @      \result == getStatements().elementAt(getStatements().indexOf(element) - 1).lexicalContext();
+   @*/
+	public LookupStrategy lexicalLookupStrategy(Element element) throws LookupException {
 		List<Statement> declarations = statements();
 		int index = declarations.indexOf(element);
-		if (index == 0) {
+		if(index == 0) {
 			return parent().lexicalLookupStrategy(this);
 		} else if (index > 0) {
-			return declarations.get(index - 1).linearLookupStrategy();
+			return declarations.get(index-1).linearLookupStrategy();
 		} else {
-			throw new ChameleonProgrammerException(
-					"Invoking lexicalContext(element) with an element that is not a child.");
+		  throw new ChameleonProgrammerException("Invoking lexicalContext(element) with an element that is not a child.");
 		}
 	}
 
@@ -113,9 +95,5 @@ public class Block extends StatementImpl<Block> implements
 		return Valid.create();
 	}
 
-	public void addBlock(Block block) {
-		for (Statement s : block.statements())
-			addStatement(s);
-	}
-
+	
 }
